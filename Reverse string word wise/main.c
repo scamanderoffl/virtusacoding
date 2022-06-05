510c1779f9995bfe0de5cb4db61d379728455cca
Balaji <59250390+scamanderoffl@users.noreply.github.com>
2022-06-05 14:59:33 +0530

    Add files via upload

---
+++ Reverse string word wise/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+
+void main()
+
+{
+    int i, j = 0, k = 0, x, len;
+    char str[100], str1[10][20], temp;
+    scanf("%[^\n]s", str);
+
+    for (i = 0;str[i] != '\0'; i++)
+    {
+        if (str[i] == ' ')
+        {
+            str1[k][j]='\0';
+            k++;
+            j=0;
+        }
+        else
+        {
+            str1[k][j]=str[i];
+            j++;
+        }
+    }
+
+    str1[k][j] = '\0';
+
+    for (i=k;i>=0;i--)
+
+    {
+        printf("%s ", str1[i]);
+    }
+}
